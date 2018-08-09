26fe42136464b7ee3b1259b7ca30e9e76304e77a
radu.iman <radu.iman@P6198.svdomain1.softvision.ro>
2018-08-09 18:04:11 +0300

    Add hg_api.py script to pull the changelog from Mozilla HG repositories and store it in hg_changelog.json file

---
+++ tests/hg_api.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+hgREPO = { 'Version-Control-Tools' : 'https://hg.mozilla.org/hgcustom/version-control-tools/json-log',
+           'Mozilla-Build' : 'https://hg.mozilla.org/mozilla-build/json-log',
+           'Tooltool' : 'https://hg.mozilla.org/build/tooltool/json-log',
+           'Mozilla-Central' : 'https://hg.mozilla.org/mozilla-central/json-log',
+           'Try' : 'https://hg.mozilla.org/try/json-log',
+           'Autoland' : 'https://hg.mozilla.org/integration/autoland/json-log',
+           'Inbound' : 'https://hg.mozilla.org/integration/mozilla-inbound/json-log',
+           'comm-esr60' : 'https://hg.mozilla.org/releases/comm-esr60/json-log',
+           'Beta' : 'https://hg.mozilla.org/releases/mozilla-beta/json-log',
+           'mozilla-esr60' : 'https://hg.mozilla.org/releases/mozilla-esr60/json-log',
+           'mozilla-release' : 'https://hg.mozilla.org/releases/mozilla-release/json-log'
+}
+changelog = {}
+for hgREPO_key in hgREPO:     # for loop to scroll through the hgREPO
+    r = requests.get(hgREPO.get(hgREPO_key))     # get infos from hgAPI page
+    p = r.json()     # turn into JSON content 
+    commit = {}
+    commit_number = 0
+    for keys in p['changesets']:
+        commit.update({ 'Name: ' : keys['user'],
+                        'Date: ' : keys['date'],
+                        'Message: ' : keys['desc'],
+                        'Node: ' : keys['node'] })
+        changelog.update({ commit_number : commit })
+        commit_number += 1
+    hgREPO.update({ hgREPO_key : changelog})
+with open('./hg_changelog.json', 'w') as fp:     # open .json file with write permission
+    json.dump(hgREPO, fp)
+
+''' Using this Json viewer " http://www.jsonviewer.com/ ", 
+        you can copy the content from github_changelog.json into RAW json data: 
+            and see all the commits '''
