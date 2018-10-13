c0b968e44890074adc8b9e963748bd9129bf572e
radu.iman <radu.iman@P6198.svdomain1.softvision.ro>
2018-10-13 15:50:15 +0300

    Rewrite the main script in python. Other quick updates.

--- tests/md_changelog.py
+++ tests/md_changelog.py
@@ -10,13 +10,9 @@ git_data = json.loads(git_logs) #add all the infos into git_data
 hg_logs=open('../tests/hg_changelog.json').read() 
 hg_data = json.loads(hg_logs)
 
-f = open('../tests/changelog.md', "a")    #open the .md file 
-changelogs_hg = {}   #hg dictionary
-changelogs_git = {}  #git dictionary
-changelogs = {}  #main dictionary
-title = "### GIT" + "\n"  #header fro GIT table
+f = open( '../tests/changelog.md', 'w')
+title = "### GIT" + "\n"  #header for GIT table
 header = "| Repository | Commiter | Email | Time | URL | Message |" + "\n" + "| --- | --- | --- | --- | --- | --- |" + "\n"  # table header
-
 f.write(title)  #write the headers into the file
 f.write(header)
 for key in git_data:              #for loop to scroll through the git data          
@@ -27,11 +23,11 @@ for key in git_data:              #for loop to scroll through the git data
         commit_date = git_data[key][keys]['Date: '] 
         commit_url = git_data[key][keys]['Url: '] 
         commit_message = git_data[key][keys]['Message: ']
-        row_line = ('|' + repository + '|' + commiter_name + '|' + commiter_email + '|' + commit_date + '|' + commit_url + '|' + commit_message + '|' + '\n')   #add each info into the row line
-        f.write(row_line)  #write the row line into the table
+        row_line = ('|' + repository + '|' + commiter_name + '|' + commiter_email + '|' + commit_date + '|' + commit_url + '|' + commit_message + '|' '\n')   #add each info into the row line
+        f.write(row_line)
+
 title = "### Mercurial" + "\n"
 header = "| Repository | Commiter | Time | Node | Message |" + "\n" + "| --- | --- | --- | --- | --- |" + "\n"   #the header for the hg table
-
 f.write(title)
 f.write(header)    #write the header into the .md file
 for key1 in hg_data:    #for loop to scroll through the hg hg data 
@@ -42,4 +38,4 @@ for key1 in hg_data:    #for loop to scroll through the hg hg data
         commit_message = hg_data[key1][keys1]['Message: ']
         commt_node = hg_data[key1][keys1]['Node: ']
         row_line = ('|' + repository + '|' + commiter_name + '|' + commit_date + '|' + commt_node + '|' + commit_message + '|' '\n')    #row line with each commit infos
-        f.write(row_line)  #write the row line into the table
+        f.write(row_line)
