5488f3f1cd976d48ce8220124efd93a4299dc638
bogdan.crisan <bogdan.18rap@gmail.com>
2018-11-19 18:05:42 +0200

    Added functions: - clear_file() - write_main_mk_table() - extract_hg_json() - extract_github_data() - generate_main_mk_table()
    Added main markdown table file.

--- client.py
+++ client.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 import json
 import re
 import requests
+from os import listdir
+from os.path import isfile, join
 
 # Create a Github instance:
 lastWeek = datetime.now() - timedelta(days=7)
@@ -216,6 +218,112 @@ def create_files_for_git():
         create_git_md_table(repository_name)
 
 
+def clear_file(file_name):
+    """
+    This function takes a file that clears the content and output's a base table header for a markdown file.
+    :param file_name: Name of the file to be written. (should also contain the path)
+    :return: A file should be created and should contain base table.
+    """
+    file = open(file_name, 'w')
+    heading = "###  Last commits from every repository"
+    base_table = '|      Repository      |                   Last commit               |    Deploy time       | \n' + \
+                 '|:--------------------:|:-------------------------------------------:|:--------------------:| \n'
+    file.write(heading + "\n" + base_table)
+    file.close()
+
+
+def write_main_mk_table(file_name, repository, last_commit, deploy_time):
+    """
+    This function opens a file (that file should be already created and should contain a markdown table header) and
+    appends to it a row that will contain the repository, the last commit and the deploy time.
+    :param file_name: Name of the file in which the content is appended. (should also contain the path)
+    :param repository: Repository name for the first element of the table.
+    :param last_commit: Description of the last commit used as the 2nd element of the table
+    :param deploy_time: Time and Time designator used as the 3rd element of the table
+    :return:
+    """
+    row = "|" + repository + \
+          "|" + last_commit + \
+          "|" + deploy_time + \
+          "|" + '\n'
+    write_file = open(file_name, "a")
+    write_file.write(row)
+
+
+def extract_hg_json(json_files):
+    """
+    Extracts the json data from json files and writes the data to the main markdown table file. The function looks
+    into json files after the last commit, extracts it and calls the write_main_mk_table function.
+
+    :param json_files: list of files to extract commits from.
+    :return: none
+    """
+    for file in json_files:
+        file_path = "hg_files/" + file
+
+        with open(file_path) as json_files:
+            data = json.load(json_files)
+            # pprint(data)
+            github_base_link = "https://github.com/Akhliskun/firefox-infra-changelog/blob/master/hg_files/"
+            repository_name = "[" + file.rstrip().replace(".json", "") + "]" + "(" + github_base_link + \
+                              file.rstrip().replace(" ", "%20") + ")"
+
+            for test in data:
+                commit_description = data.get(test)["commit_message"]
+                commit_date = data.get(test)["commit_date"]
+                # commit_url = data.get(test)["url"]  # TODO add link to mk table
+                write_main_mk_table("main_mk_table.md", repository_name, commit_description, commit_date)
+                # We are braking this for loop since we got the last commit.
+                break
+
+
+def extract_github_data(json_files):
+    """
+    Extracts the json data from json files and writes the data to the main markdown table file. The function looks
+    into json files after the last commit, extracts it and calls the write_main_mk_table function.
+
+    :param json_files: list of files to extract commits from.
+    :return: none
+    """
+    for file in json_files:
+        file_path = "git_files/" + file
+
+        with open(file_path) as json_files:
+            data = json.load(json_files)
+            github_base_link = "https://github.com/Akhliskun/firefox-infra-changelog/blob/master/git_files/"
+            repository_name = "[" + file.rstrip().replace(".json", "") + "]" + "(" + github_base_link + \
+                              file.rstrip().replace(" ", "%20").rstrip().replace(".json", ".md") + ")"
+
+            for test in data:
+                commit_description = data.get(test)["commit_message"]
+                commit_date = data.get(test)["commit_date"]
+                # commit_url = data.get(test)["url"]  # TODO add link to mk table
+                write_main_mk_table("main_mk_table.md", repository_name, commit_description, commit_date)
+                # We are braking this for loop since we got the last commit.
+                break
+
+
+def generate_main_mk_table():
+    """
+    Looks into repositories folders (hg_files & git files), filters the files to load the json's using a passfilter and
+    calls after extraction functions.
+    """
+    # Get current folder path.
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Look into repositories folder and list all of the files
+    hg_only_files = [f for f in listdir(dir_path + "/hg_files") if isfile(join(dir_path + "/hg_files", f))]
+    git_only_files = [f for f in listdir(dir_path + "/git_files") if isfile(join(dir_path + "/git_files", f))]
+
+    # Pass filter only the ".json" objects
+    hg_json_files = [jf for jf in hg_only_files if ".json" in jf]
+    git_json_files = [jf for jf in git_only_files if ".json" in jf]
+
+    # Extract data from json_files and writes to main markdown table.
+    extract_hg_json(hg_json_files)
+    extract_github_data(git_json_files)
+
+
 if __name__ == "__main__":
     TOKEN = os.environ.get("GIT_TOKEN")
     git = Github(TOKEN)
@@ -223,4 +331,6 @@ if __name__ == "__main__":
     repositories = json.loads(repositories_data)
     create_files_for_git()
     create_files_for_hg()
+    clear_file("main_mk_table.md")
+    generate_main_mk_table()
 
