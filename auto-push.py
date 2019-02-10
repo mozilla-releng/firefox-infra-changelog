e529f1fa2d0575dd951503ad69666740acaee39b
radu.iman <radu.iman@softvision.ro>
2019-02-10 16:16:19 +0200

    - added auto-push.py. The script to auto push data on gitHub.

---
+++ auto-push.py
@@ -0,0 +1,10 @@
+import subprocess
+import datetime
+
+subprocess.call(['git', 'add', 'git_files/*'])
+subprocess.call(['git', 'add', 'hg_files/*'])
+subprocess.call(['git', 'add', 'main_md_table.md'])
+commit_message = "Changelog:  " + str(datetime.datetime.now()) + \
+                    "\n - updated git files \n - updated hg files \n - updated main_md_table"
+subprocess.call(['git', 'commit', '-m', commit_message])
+subprocess.call(['git', 'push', 'origin', 'auto-generated-files'])
