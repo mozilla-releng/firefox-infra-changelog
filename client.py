8554724ff79efa9c8d25757f0f63a6a43e50a23e
Akhliskun <labicidanut@gmail.com>
2019-02-27 17:52:08 +0200

    Implement changes from PR 213

--- client.py
+++ client.py
@@ -15,58 +15,50 @@ from fic_modules.configuration import (
 from fic_modules.markdown_modules import generate_main_md_table
 
 
-@click.group()
-def cli():
+@click.command()
+@click.option('-g', '--git', is_flag=True, flag_value='git',
+              help='Run only for GIT repos')
+@click.option('-hg', '--hg', is_flag=True, flag_value='hg',
+              help='Run only for HG repos')
+@click.option('-l', '--logger', is_flag=True, flag_value='logger',
+              help='Display logger')
+@click.option('-m', '--multiple', is_flag=True, flag_value='multiple',
+              help='Let you choose for which repositories the script will run')
+@click.option('-a', '--all', is_flag=True, flag_value='all',
+              help='Run for all currently available repositories')
+@click.help_option('-h', '--help')
+def cli(all, git, hg, logger, multiple):
+    from fic_modules.configuration import LOGGER
     """Firefox-Infra-Changelog: tool which build a
     changelog of commits happening on git or hg that
     could affect Firefox CI Infra"""
-    pass
-
-
-@cli.command()
-@click.option("--all", flag_value="a",
-              help="Run for all currently available repositories")
-@click.option("--git", is_flag=True, flag_value='git',
-              help="Run only for GIT repos")
-@click.option("--hg", is_flag=True, flag_value='hg',
-              help="Run only for HG repos")
-@click.option("--l", is_flag=True, flag_value="l",
-              help="Display logger")
-@click.option("--m", is_flag=True, flag_value="m",
-              help="Let you choose for which repositories the script will run")
-def cli(all, git, hg, l, m):
-    from fic_modules.configuration import LOGGER
-    """
-    Firefox-Infra-Changelog: tool which build a
-    changelog of commits happening on git or hg that
-    could affect Firefox CI Infra"""
-    if l:
+    if logger:
         logging.getLogger().addHandler(logging.StreamHandler())
     if all:
-        LOGGER.info("========Logging in ALL mode on %s ========",
-                    datetime.now())
+        LOGGER.info("======== Logging in ALL mode on %s ========", datetime
+                    .now())
         create_files_for_git(REPOSITORIES, onerepo=False)
         create_files_for_hg(REPOSITORIES, onerepo=False)
         clear_file("changelog.md", GENERATE_FOR_X_DAYS)
         generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
         generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
     if git:
-        LOGGER.info("========Logging in GIT mode on %s ========",
-                    datetime.now())
+        LOGGER.info("======== Logging in GIT mode on %s ========", datetime
+                    .now())
         create_files_for_git(REPOSITORIES, onerepo=False)
         clear_file("changelog.md", GENERATE_FOR_X_DAYS)
         generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
         generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
         click.echo("Script ran in GIT Only mode")
     if hg:
-        LOGGER.info("========Logging in HG mode on %s ========",
-                    datetime.now())
+        LOGGER.info("======== Logging in HG mode on %s ========",datetime
+                    .now())
         create_files_for_hg(REPOSITORIES, onerepo=False)
         clear_file("changelog.md", GENERATE_FOR_X_DAYS)
         generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
         generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
         click.echo("Script ran in HG Only mode")
-    if m:
+    if multiple:
         get_keys("Github")
         get_keys("Mercurial")
         for scriptrepo in REPOSITORIES.get("Github").get("build-puppet")\
@@ -82,8 +74,9 @@ def cli(all, git, hg, l, m):
                                 "number, "
                                 "type q when you are done: ")
             if str(user_choice) == "q":
-                LOGGER.info("========Logging for %s on %s ========",
-                            str(new_list).strip('[]'), datetime.now())
+                LOGGER.info('========Logging for %s on %s ========'
+                            , str(new_list).strip('[]'), datetime
+                            .now())
                 for repository in new_list:
                     if repository in REPOSITORIES.get("Github"):
                         create_files_for_git(repository, onerepo=True)
@@ -100,7 +93,7 @@ def cli(all, git, hg, l, m):
             try:
                 new_entry = int(user_choice) - 1
                 if new_entry < 0 or new_entry >= len(REPO_LIST):
-                    print('Not Valid')
+                    print('Choice not valid!')
                 else:
                     new_list.append(REPO_LIST[int(new_entry)])
                     REPO_LIST.pop(int(new_entry))
