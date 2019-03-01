7bacd744edeac87b99f986dc60d0634b665d19a7
bogdan.crisan <bogdan.18rap@gmail.com>
2019-03-01 06:12:08 +0200

    Created separated function for each run type and removed the described in issue #227

--- client.py
+++ client.py
@@ -15,6 +15,79 @@ from fic_modules.configuration import (
 from fic_modules.markdown_modules import generate_main_md_table
 
 
+def run_all(logger):
+    logger.info("======== Logging in ALL mode on %s ========", datetime
+                .now())
+    create_files_for_git(REPOSITORIES, onerepo=False)
+    create_files_for_hg(REPOSITORIES, onerepo=False)
+    clear_file("changelog.md", GENERATE_FOR_X_DAYS)
+    generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
+    generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
+
+
+def run_git(logger):
+    logger.info("======== Logging in GIT mode on %s ========", datetime
+                .now())
+    create_files_for_git(REPOSITORIES, onerepo=False)
+    clear_file("changelog.md", GENERATE_FOR_X_DAYS)
+    generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
+    generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
+    click.echo("Script ran in GIT Only mode")
+
+
+def run_hg(logger):
+    logger.info("======== Logging in HG mode on %s ========", datetime
+                .now())
+    create_files_for_hg(REPOSITORIES, onerepo=False)
+    clear_file("changelog.md", GENERATE_FOR_X_DAYS)
+    generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
+    generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
+    click.echo("Script ran in HG Only mode")
+
+
+def run_multiple(logger):
+    get_keys("Github")
+    get_keys("Mercurial")
+    for scriptrepo in REPOSITORIES.get("Github").get("build-puppet") \
+            .get("configuration").get("files-to-check"):
+        REPO_LIST.append(scriptrepo)
+    new_list = []
+    while input != "q":
+        print("You have selected : ", new_list)
+        for keys in REPO_LIST:
+            print(REPO_LIST.index(keys) + 1, keys)
+
+        user_choice = input("Select a repo by typing it's "
+                            "number, "
+                            "type q when you are done: ")
+        if str(user_choice) == "q":
+            logger.info('========Logging for %s on %s ========'
+                        , str(new_list).strip('[]'), datetime
+                        .now())
+            for repository in new_list:
+                if repository in REPOSITORIES.get("Github"):
+                    create_files_for_git(repository, onerepo=True)
+                    generate_main_md_table("git_files",
+                                           GENERATE_FOR_X_DAYS)
+                elif repository in REPOSITORIES.get("Mercurial"):
+                    create_files_for_hg(repository, onerepo=True)
+                    clear_file("changelog.md",
+                               GENERATE_FOR_X_DAYS)
+                    generate_main_md_table("hg_files",
+                                           GENERATE_FOR_X_DAYS)
+                    generate_main_md_table("git_files",
+                                           GENERATE_FOR_X_DAYS)
+        try:
+            new_entry = int(user_choice) - 1
+            if new_entry < 0 or new_entry >= len(REPO_LIST):
+                print('Choice not valid!')
+            else:
+                new_list.append(REPO_LIST[int(new_entry)])
+                REPO_LIST.pop(int(new_entry))
+        except ValueError:
+            exit(0)
+
+
 @click.command()
 @click.option('-g', '--git', is_flag=True, flag_value='git',
               help='Run only for GIT repos')
@@ -35,70 +108,13 @@ def cli(all, git, hg, logger, multiple):
     if logger:
         logging.getLogger().addHandler(logging.StreamHandler())
     if all:
-        LOGGER.info("======== Logging in ALL mode on %s ========", datetime
-                    .now())
-        create_files_for_git(REPOSITORIES, onerepo=False)
-        create_files_for_hg(REPOSITORIES, onerepo=False)
-        clear_file("changelog.md", GENERATE_FOR_X_DAYS)
-        generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
-        generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
+        run_all(LOGGER)
     if git:
-        LOGGER.info("======== Logging in GIT mode on %s ========", datetime
-                    .now())
-        create_files_for_git(REPOSITORIES, onerepo=False)
-        clear_file("changelog.md", GENERATE_FOR_X_DAYS)
-        generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
-        generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
-        click.echo("Script ran in GIT Only mode")
+        run_git(LOGGER)
     if hg:
-        LOGGER.info("======== Logging in HG mode on %s ========",datetime
-                    .now())
-        create_files_for_hg(REPOSITORIES, onerepo=False)
-        clear_file("changelog.md", GENERATE_FOR_X_DAYS)
-        generate_main_md_table("hg_files", GENERATE_FOR_X_DAYS)
-        generate_main_md_table("git_files", GENERATE_FOR_X_DAYS)
-        click.echo("Script ran in HG Only mode")
+        run_hg(LOGGER)
     if multiple:
-        get_keys("Github")
-        get_keys("Mercurial")
-        for scriptrepo in REPOSITORIES.get("Github").get("build-puppet")\
-                .get("configuration").get("files-to-check"):
-            REPO_LIST.append(scriptrepo)
-        new_list = []
-        while input != "q":
-            print("You have selected : ", new_list)
-            for keys in REPO_LIST:
-                print(REPO_LIST.index(keys) + 1, keys)
-
-            user_choice = input("Select a repo by typing it's "
-                                "number, "
-                                "type q when you are done: ")
-            if str(user_choice) == "q":
-                LOGGER.info('========Logging for %s on %s ========'
-                            , str(new_list).strip('[]'), datetime
-                            .now())
-                for repository in new_list:
-                    if repository in REPOSITORIES.get("Github"):
-                        create_files_for_git(repository, onerepo=True)
-                        generate_main_md_table("git_files",
-                                               GENERATE_FOR_X_DAYS)
-                    elif repository in REPOSITORIES.get("Mercurial"):
-                        create_files_for_hg(repository, onerepo=True)
-                        clear_file("changelog.md",
-                                   GENERATE_FOR_X_DAYS)
-                        generate_main_md_table("hg_files",
-                                               GENERATE_FOR_X_DAYS)
-                        generate_main_md_table("git_files",
-                                               GENERATE_FOR_X_DAYS)
-            try:
-                new_entry = int(user_choice) - 1
-                if new_entry < 0 or new_entry >= len(REPO_LIST):
-                    print('Choice not valid!')
-                else:
-                    new_list.append(REPO_LIST[int(new_entry)])
-                    REPO_LIST.pop(int(new_entry))
-            except ValueError:
-                exit(0)
+        run_multiple(LOGGER)
 
 
 if __name__ == "__main__":
