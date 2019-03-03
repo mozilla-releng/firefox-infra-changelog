e7c515cd1249c60921a22cb2876deef44b5fe7a4
Akhliskun <labicidanut@gmail.com>
2019-03-04 00:37:56 +0200

    Re-add Invalid Bug logger message

--- fic_modules/helper_functions.py
+++ fic_modules/helper_functions.py
@@ -246,7 +246,8 @@ def replace_bug_with_url(message, LOGGER):
                 commit_text[element] = '[' + 'Bug' + ' ' + str(bug_number)
                 commit_text[element + 1] = '](' + generated_link + ')'
             except ValueError:
-                pass
+                LOGGER.warning("Invalid bug number: > {} < in message: {}"
+                               .format(commit_text[element + 1], message))
     commit_text = ' '.join(commit_text)
     return commit_text
 
