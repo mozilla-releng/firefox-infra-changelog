4c84b773acbe576e638e2e67985c371441afff1e
Akhliskun <labicidanut@gmail.com>
2019-04-10 17:11:27 +0300

    Add basic config defaults

---
+++ modules/config.py
@@ -0,0 +1,9 @@
+# Common paths
+REPOSIRIES_FILE = "./repositories.json"
+GIT_TOKEN       = "GIT_TOKEN"
+
+# Default values, if nothing is offered, fall back to these.
+DEFAULT_REPO_CHOICE       = "ALL"
+HG_CHANGESETS_TO_SHOW     = int(100)
+COMMIT_DESCRIPTION_LENGTH = int(400)
+DEFAULT_LOGGING_LEVEL     = int(10)
