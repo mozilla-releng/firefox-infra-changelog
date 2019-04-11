6605128151dfda3e2c69d5a5baa8c8758f95ce23
radu.iman <radu.iman@softvision.ro>
2019-04-11 16:45:31 +0300

    Add LOG_FILE_PATH and changed DEFAULT_LOGGING_LEVEL
    - log file path will contain the path to the file to be used for logging
    - default logging level will now expect a string instead of int

--- modules/config.py
+++ modules/config.py
@@ -3,10 +3,12 @@ REPOSIRIES_FILE     = "./repositories.json"
 CHANGELOG_JSON_PATH = "./changelog.json"
 CHANGELOG_MD_PATH   = "./changelog.md"
 CHANGELOG_REPO_PATH = "./data/"
+LOG_FILE_PATH       = "./LOG.log"
 GIT_TOKEN           = "GIT_TOKEN"  # Name of OS.ENV from which to pull the Github Token
 
 # Default values, if nothing is offered, fall back to these.
 DEFAULT_REPO_CHOICE       = "ALL"
 HG_CHANGESETS_TO_SHOW     = int(100)
 COMMIT_DESCRIPTION_LENGTH = int(400)
-DEFAULT_LOGGING_LEVEL     = int(10)
+DEFAULT_LOGGING_LEVEL     = str("INFO")
+
