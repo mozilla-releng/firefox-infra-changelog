295efc81eb5fa4bcd2481ca463a5d0f5df8832fd
radu.iman <radu.iman@softvision.ro>
2019-04-25 05:38:25 +0300

    Add 'GIT_TOKEN' to GIT_TOKEN list from config.py

--- modules/config.py
+++ modules/config.py
@@ -4,7 +4,7 @@ CHANGELOG_JSON_PATH = "./changelog.json"
 CHANGELOG_MD_PATH   = "./changelog.md"
 CHANGELOG_REPO_PATH = "./data/"
 LOG_FILE_PATH       = "./LOG.log"
-GIT_TOKEN           = []   # Name of OS.ENV from which to pull the Github Token
+GIT_TOKEN           = ['GIT_TOKEN']   # Name of OS.ENV from which to pull the Github Token
 
 # Default values, if nothing is offered, fall back to these.
 DEFAULT_REPO_CHOICE       = "ALL"
