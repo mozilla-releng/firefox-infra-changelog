10089f8af5b536c37ef4ed3e42ca8c7a45670c63
radu.iman <radu.iman@softvision.ro>
2019-05-19 06:04:44 +0300

    Write the logic of the run_fic() method

--- modules/FIC_Core.py
+++ modules/FIC_Core.py
@@ -2,6 +2,7 @@ from modules.FIC_Github import FICGithub
 from modules.FIC_Mercurial import FICMercurial
 from modules.FIC_FileHandler import FICFileHandler
 from modules.FIC_Logger import FICLogger
+import json
 
 
 class FICCore(FICGithub, FICMercurial, FICFileHandler, FICLogger):
@@ -10,22 +11,37 @@ class FICCore(FICGithub, FICMercurial, FICFileHandler, FICLogger):
         FICMercurial.__init__(self)
         FICFileHandler.__init__(self)
         FICLogger.__init__(self)
+        self.check_tool_integrity()
 
     def run_fic(self, all=False, git_only=False, hg_only=False, repo_list=None, days=3):
         # Don't forget about days!
-
         if all:
-            pass
+            for hosting_service in json.load(self.load(None, "repositories.json")):
+                for repo in json.load(self.load(None, "repositories.json")).get(hosting_service):
+                    self.repo_name = repo
+                    self.start_git() if hosting_service == "Github" else self.start_hg()
 
         if git_only:
-            pass
+            for repo in json.load(self.load(None, "repositories.json")).get("Github"):
+                self.repo_name = repo
+                self.start_git()
 
         if hg_only:
-            pass
+            for repo in json.load(self.load(None, "repositories.json")).get("HG"):
+                self.repo_name = repo
+                self.start_hg()
 
         if repo_list:
-            pass
+            for repo in repo_list:
+                self.repo_name = repo
+                try:
+                    self.start_git()
+                except TypeError:
+                    self.start_hg()
 
     def _markdown(self):
-        pass
+        self.git_markdown()
+        self.hg_markdown()
+        self.main_markdown()
+
 
