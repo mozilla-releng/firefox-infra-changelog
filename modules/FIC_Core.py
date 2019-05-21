4abbd510026ad88bea598106bd409c8979cfb69a
Akhliskun <labicidanut@gmail.com>
2019-05-21 14:59:58 +0300

    Fix some typos

--- modules/FIC_Core.py
+++ modules/FIC_Core.py
@@ -20,26 +20,26 @@ class FICCore(FICGithub, FICMercurial, FICFileHandler, FICLogger):
             # Needs to be replaced with whatever we want the script to do.
             # In this case, with a method that should run the script in all modes.
             print("Changelog has run in All mode!")
-            self._run_all_behavioral()
+            self._run_all_behavior()
 
         if git_only:
             print("Changelog has run in git only mode!")
-            self._run_git_behavioral()
+            self._run_git_behavior()
 
         if hg_only:
             print("Changelog has run in hg only mode!")
-            self._run_hg_behavioral()
+            self._run_hg_behavior()
 
         if repo_list:
             print("Changelog has run with a custom list of repositories!")
-            self._run_custom_repos_behavioral(repo_list)
+            self._run_custom_repos_behavior(repo_list)
 
     def _markdown(self):
         self.git_markdown()
         self.hg_markdown()
         self.main_markdown()
 
-    def _run_all_behavioral(self):
+    def _run_all_behavior(self):
         # Describes the behavioral of the script that runs in all mode.
         print("Testing run all behavioral...")
 
@@ -50,21 +50,21 @@ class FICCore(FICGithub, FICMercurial, FICFileHandler, FICLogger):
                 else:
                     self.start_hg(repo)
 
-    def _run_git_behavioral(self):
+    def _run_git_behavior(self):
         # Describes the behavioral of the script that runs in git only mode.
         print("Testing git mode behavioral...")
 
         for repo in json.load(self.load(None, "repositories.json")).get("Github"):
             self.start_git(repo)
 
-    def _run_hg_behavioral(self):
+    def _run_hg_behavior(self):
         # Describes the behavioral of the script that runs in hg only mode.
         print("Testing hg mode behavioral...")
 
         for repo in json.load(self.load(None, "repositories.json")).get("HG"):
             self.start_hg(repo)
 
-    def _run_custom_repos_behavioral(self, repo_list):
+    def _run_custom_repos_behavior(self, repo_list):
         # Describes the behavioral of the script that runs with custom repos mode.
         print("Testing custom repositories mode behavioral...")
 
