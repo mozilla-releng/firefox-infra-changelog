78333b2338e51eba2bfa510c11712faebca05cb3
Zsolt <zfay@mozilla.com>
2019-05-22 06:36:53 +0300

    added MD handling for individual git repos

--- plugins/FIC_Markdown.py
+++ plugins/FIC_Markdown.py
@@ -4,31 +4,38 @@
 from modules.FIC_DataVault import FICDataVault
 from modules.FIC_FileHandler import FICFileHandler
 from modules.FIC_Utilities import return_time
-from modules.config import COMMIT_DESCRIPTION_LENGTH
+from modules.config import COMMIT_DESCRIPTION_LENGTH, CHANGELOG_REPO_PATH
+import json
 
 
 class FICMarkdownGenerator(FICFileHandler, FICDataVault):
     def __init__(self):
         FICFileHandler.__init__(self)
         FICDataVault.__init__(self)
-        self.changelog_table_header = None
+        self.md_ready_data = []
 
-    def _get_current_time(self):
-        return return_time()
+    @staticmethod
+    def _get_current_time():
+        return return_time(time_format="%Y-%m-%dT%H:%M:%S")
+
+    def _load_local_json_data(self):
+        return json.load(self.load(CHANGELOG_REPO_PATH, self.repo_name.lower() + ".json"))
+
+    def _build_initial_md_structure(self):
+        self.md_ready_data.append(self._create_repo_markdown_header())
+        self.md_ready_data.append(self._create_first_table_row())
 
     def _create_repo_markdown_header(self):
-        return self.repo_name + " MD table" + "\n" + "Generated on: {}".format(self._get_current_time())
+        return "## " + self.repo_name + " MD table" + "\n" + "Generated on: {}".format(self._get_current_time()) + "\n\n"
 
-    def _create_first_table_row(self):
+    @staticmethod
+    def _create_first_table_row():
         return "| Commit Number | Commiter | Commit Message | Commit Url | Date | \n" + \
-               "|:-----:|:-----:|:----------------------------------:|:------:|:----:| \n"
+               "|:-----:|:-----:|:----------------------------------:|:------:|:----:| \n\n"
 
     def md_table_row_builder(self):
         return "|" + str(self.commit_number) + "|" + self.commit_author + "|" + self.commit_message + \
                "|" + "[URL](" + self.commit_url + ")" + "|" + str(self.commit_date) + "\n"
-
-    def write_markdown(self, directory, file_name):
-        self.save(directory, file_name, "CONTENT HERE")
 
     def generate_link_for_bugs(self):
         import re
@@ -79,7 +86,35 @@ class FICMarkdownGenerator(FICFileHandler, FICDataVault):
             self.commit_message = self.commit_message[0:length] + ".. [continue reading](" + commit_link + ")"
         return self.commit_message
 
-    def filter_commit(self):
-        self.filter_strings()
-        self.trim_commit_description(self.commit_url)
-        self.generate_link_for_bugs()
+    def _populate_md_table(self):
+        local_json_data = self._load_local_json_data()
+        if len(local_json_data) > 0:
+            del local_json_data["0"]
+            self.commit_number = 1
+            for key in local_json_data:
+                self.commit_date = local_json_data.get(key).get("date")
+                if self.commit_date > return_time("%Y-%m-%dT%H:%M:%S", "sub", 7):
+                    self.commit_author = local_json_data.get(key).get("author")
+                    self.commit_url = local_json_data.get(key).get("url")
+                    self.commit_message = local_json_data.get(key).get("message")
+                    self.commit_author, self.commit_message = self.filter_strings()
+                    self.trim_commit_description(self.commit_url, COMMIT_DESCRIPTION_LENGTH)
+                    self.generate_link_for_bugs()
+                    self.md_ready_data.append(self.md_table_row_builder())
+                    self.commit_number += 1
+        else:
+            print("No commits in the past 7 days")  # to be replace with a method
+
+    def start_md_for_git(self, repo_name=None):
+        self.repo_name = repo_name
+        self._build_initial_md_structure()
+        self._populate_md_table()
+        for element in a.md_ready_data:
+            print(element)
+            a.save(CHANGELOG_REPO_PATH, a.repo_name + ".md", element)
+
+
+# TESTING BELOW
+
+a = FICMarkdownGenerator()
+a.start_md_for_git("taskcluster")
