d39b48815095022e8bd619f5fb022627937ffc09
Labici Danut <labicidanut@gmail.com>
2019-05-29 16:52:23 +0300

    Fix typo in config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
 # Common paths
-REPOSIRIES_FILE     = "repositories.json"
+REPOSITORIES_FILE   = "repositories.json"
 CHANGELOG_JSON_PATH = "changelog.json"
 CHANGELOG_MD_PATH   = "changelog.md"
 CHANGELOG_REPO_PATH = "data"
