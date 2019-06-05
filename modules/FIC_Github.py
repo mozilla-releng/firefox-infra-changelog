6831a039b68032b912ee7c32cfc5bada7af443a3
radu.iman <radu.iman@softvision.ro>
2019-06-05 18:48:58 +0300

    [Github] Improve the ability to store the data for a specific amount of time

--- modules/FIC_Github.py
+++ modules/FIC_Github.py
@@ -299,11 +299,11 @@ class FICGithub(FICFileHandler, FICDataVault):
         self._write_git_json()
 
     def _check_commit_age(self, filter_list=None):
-        filter_list = self.list_of_commits if filter_list == None else filter_list
-        for element in range(1, len(filter_list)):
-            commit_time = return_time(input_time=filter_list[element]["date"], input_time_format="%Y-%m-%dT%H:%M:%SZ")
-            if commit_time > return_time(operation="sub", operation_days=INDIVIDUAL_REPO_DAYS):
-                del filter_list[element]
+        for commit in range(1, len(filter_list)):
+            if return_time(input_time=filter_list[str(commit)]["date"], input_time_format="%Y-%m-%dT%H:%M:%SZ") > \
+               return_time(operation="sub", operation_days=INDIVIDUAL_REPO_DAYS):
+                self.commit_number += 1
+                self.list_of_commits.update({str(self.commit_number): filter_list[str(commit)]})
 
     def _write_git_json(self):
         local_json_data = json.load(self.load(CHANGELOG_REPO_PATH, self.repo_name.lower() + ".json"))
@@ -311,16 +311,12 @@ class FICGithub(FICFileHandler, FICDataVault):
         if len(self.list_of_commits) == 1:
             local_json_data.update(self._generate_first_element())
             self._check_commit_age(local_json_data)
-            self.save(CHANGELOG_REPO_PATH, self.repo_name + ".json", local_json_data)
+            self.save(CHANGELOG_REPO_PATH, self.repo_name + ".json", self.list_of_commits)
         # In case we have new commits + local data
         elif len(local_json_data) >= 1:
             local_json_data.pop("0")
-            for commit in range(len(local_json_data)):
-                self.commit_number += 1
-                self.list_of_commits.update({str(self.commit_number): local_json_data[str(commit + 1)]})
-            self._check_commit_age()
+            self._check_commit_age(local_json_data)
             self.save(CHANGELOG_REPO_PATH, self.repo_name + ".json", self.list_of_commits)
         # In case we have new commits and NO local data
         else:
-            self._check_commit_age()
             self.save(CHANGELOG_REPO_PATH, self.repo_name + ".json", self.list_of_commits)
